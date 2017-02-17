16e90c989471db2d91f9eeb741794ca954af0f33
Tom Giordano <justom@gmail.com>
2017-02-16 22:28:26 -0500

    Version using callbacks.

---
+++ ModuleExercise/solve-cb.js
@@ -0,0 +1,24 @@
+var rect = require('./rectangle-cb');
+
+function printRectInfo(l,b) {
+    console.log("Printing info for rectangle of dim. (" + l + "," + b + ")");
+    rect(l,b, function(err, rectangle) {
+        if (err) {
+            console.log("This should be fine, right?");
+            console.log(err);
+        }
+        else {
+            console.log("Area of rectangle of dim. (" + l + "," + b + ") = "  
+                        + rectangle.area());
+            console.log("Perimeter of rectangle of dim. (" + l + "," + b + ") = "  
+                        + rectangle.perimeter());
+        }
+    })
+}
+
+
+printRectInfo(2,4);
+printRectInfo(3,5);
+printRectInfo(-3,5);
+printRectInfo(222,5.9999);
+
